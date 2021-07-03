98be0e87c5a04887a02cd42d209d5ce79ca2050c
butler-yun <myung5722@naver.com>
2021-07-03 18:22:45 +0900

    신규 아이디 추천

--- level1/신규_아이디_추천.js
+++ level1/신규_아이디_추천.js
@@ -1,3 +1,4 @@
+// Answer 1
 function solution(new_id) {
     let id = new_id.toLowerCase() // 1. 대문자를 소문자로 변환
              .replace(/[^\w-_\d. ]/g, '') // 2. 사용할 수 없는 특수문자 제거
@@ -26,4 +27,17 @@ function solution(new_id) {
     }
     
     return id;
+}
+
+
+// Answer 2 리팩토링
+function solution(new_id) {
+    const id = new_id.toLowerCase() // 1. 소문자 처리
+    .replace(/[^\w-_\d. ]/g, '') // 2. 사용할 수 없는 특수문자 제거
+    .replace(/[.]+/g, '.') // 3. '..'을 '.'으로
+    .replace(/(^[.]{1,}|[.]{1,}$)/g, '') // 4. 문자열의 앞과 뒤에 있는 '.' 삭제
+    .padEnd(1, 'a') // 5. 공백문자 ' '를 문자열 'a'로 변환
+    .substr(0, 15)  // 6. 문자열을 15자리로
+    .replace(/[.]$/, ''); // 문자열을 자른 뒤 콤마 제거
+    return id.length <= 2 ? id.padEnd(3, id[id.length-1]) : id;
 }
